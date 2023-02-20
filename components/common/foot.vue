0a08977789ec07d4c8812a81c5b6540c6b11395b
弗兰克 <1085068573@qq.com>
2023-02-20 08:58:26 +0800

    footer

--- components/common/foot.vue
+++ components/common/foot.vue
@@ -1,23 +1,34 @@
 <template>
   <div class="main_footer">
-    <div class="section ">
+    <div class="section">
       <div>
         <ul>
           <li>
             <a href="https://www.facebook.com/dental.cmer/" class="Facebook">
-              <img src="https://static.cmereye.com/static/lkximg/image/common/FB.svg" alt="">
+              <img
+                src="https://static.cmereye.com/static/lkximg/image/common/FB.svg"
+                alt=""
+              />
             </a>
           </li>
           <li>
-             <a href="https://www.instagram.com/cmerdental/" class="IG">
-            <img src="https://static.cmereye.com/static/lkximg/image/common/IG.svg" alt="">
+            <a href="https://www.instagram.com/cmerdental/" class="IG">
+              <img
+                src="https://static.cmereye.com/static/lkximg/image/common/IG.svg"
+                alt=""
+              />
             </a>
           </li>
           <li>
-             <a href="https://www.youtube.com/channel/UChTznjEW5KXzeubsUeW91tQ/videos" class="Youtube">
-            <img src="https://static.cmereye.com/static/lkximg/image/common/YT.svg" alt="">
+            <a
+              href="https://www.youtube.com/channel/UChTznjEW5KXzeubsUeW91tQ/videos"
+              class="Youtube"
+            >
+              <img
+                src="https://static.cmereye.com/static/lkximg/image/common/YT.svg"
+                alt=""
+              />
             </a>
-
           </li>
         </ul>
       </div>
@@ -25,148 +36,143 @@
         <span>
           <nuxt-link to="/policy#A1">私隱政策</nuxt-link>
         </span>
-        &nbsp;
-        &nbsp;
-        &nbsp;
-        <span >
+        &nbsp; &nbsp; &nbsp;
+        <span>
           <!-- <nuxt-link to="/policy#A2">免責條款</nuxt-link> -->
           <a href="/policy#A2">免責條款</a>
-          </span>
+        </span>
       </div>
-      <div class="copyright long" >©2022 希瑪牙科（希瑪醫療集團旗下牙科中心）</div>
-      
+      <div class="copyright long">
+        ©2023 希瑪牙科（希瑪醫療集團旗下牙科中心）
+      </div>
     </div>
   </div>
 </template>
 <script>
 export default {
   data() {
-    return {
-    };
+    return {};
   },
-  created() { },
+  created() {},
   methods: {
-  //   policy(){
-  //     this.$router.push({
-  //           path:"/policy",
-  //           query:{
-            	
-  //           }
-  //       })
-  //   }
-   }
-}
+    //   policy(){
+    //     this.$router.push({
+    //           path:"/policy",
+    //           query:{
+    //           }
+    //       })
+    //   }
+  },
+};
 </script>
 <style lang="scss" scoped>
 a {
-    color: #57B2F3;
-    text-decoration: none;
-} 
+  color: #57b2f3;
+  text-decoration: none;
+}
 @media only screen and (min-width: 760px) {
-.section {
-  ul {
-    padding: 0;
-    display: flex;
-    text-align: center;
-    justify-content: center;
+  .section {
+    ul {
+      padding: 0;
+      display: flex;
+      text-align: center;
+      justify-content: center;
 
-    li {
-      margin: 2vw 2vw;
-      height: 100px;
-      width: 100px;
-      line-height: 100px;
-
+      li {
+        margin: 2vw 2vw;
+        height: 100px;
+        width: 100px;
+        line-height: 100px;
+      }
     }
   }
-}
-.main_footer {
-  background: #ECF7FC;
-  padding: 5.5vw 0 2vw;
-  .section {
-    margin: 0 auto;
-    img {
-      width: 3vw;
-    }
-    .main_menu {
-      letter-spacing: 0.2vw;
+  .main_footer {
+    background: #ecf7fc;
+    padding: 5.5vw 0 2vw;
+    .section {
+      margin: 0 auto;
+      img {
+        width: 3vw;
+      }
+      .main_menu {
+        letter-spacing: 0.2vw;
 
-      .child_menu {
-        li {
-          margin: 1vw 0;
-          &:hover {
-            color: #4570b6;
+        .child_menu {
+          li {
+            margin: 1vw 0;
+            &:hover {
+              color: #4570b6;
+            }
           }
         }
       }
-    }
-    .copyright {
-      cursor:pointer;
-      display: flex;
-      justify-content: center;
-      font-size: 0.9vw;
-      margin-top: 1vw;
-      color: #57B2F3;
-      span {
-        border-bottom: 1px solid;
+      .copyright {
+        cursor: pointer;
+        display: flex;
+        justify-content: center;
+        font-size: 0.9vw;
+        margin-top: 1vw;
+        color: #57b2f3;
+        span {
+          border-bottom: 1px solid;
+        }
       }
     }
-  }
   }
 }
 @media only screen and (max-width: 760px) {
   .section {
-  ul {
-    padding: 0;
-    display: flex;
-    text-align: center;
-    justify-content: center;
+    ul {
+      padding: 0;
+      display: flex;
+      text-align: center;
+      justify-content: center;
 
-    li {
-      margin: 2vw 2vw;
-      height: 100px;
-      width: 100px;
-      line-height: 100px;
-
+      li {
+        margin: 2vw 2vw;
+        height: 100px;
+        width: 100px;
+        line-height: 100px;
+      }
     }
   }
-}
-.main_footer {
-  background: #f3fcfe;
-  padding: 5.5vw 0 2vw;
-  padding-bottom: 20vw;
+  .main_footer {
+    background: #f3fcfe;
+    padding: 5.5vw 0 2vw;
+    padding-bottom: 20vw;
 
-  .section {
-    margin: 0 auto;
-    img {
-      width: 11vw;
-    }
-    .main_menu {
-      letter-spacing: 0.2vw;
+    .section {
+      margin: 0 auto;
+      img {
+        width: 11vw;
+      }
+      .main_menu {
+        letter-spacing: 0.2vw;
 
-      .child_menu {
-        li {
-          margin: 1vw 0;
-          &:hover {
-            color: #4570b6;
+        .child_menu {
+          li {
+            margin: 1vw 0;
+            &:hover {
+              color: #4570b6;
+            }
           }
         }
       }
-    }
-    .copyright {
-      display: flex;
-      color: #57B2F3;
-      justify-content: center;
-      font-size: 14px;
-      margin-top: 1vw;
-      span {
-        border-bottom: 1px solid;
-        font-size: 17px;
+      .copyright {
+        display: flex;
+        color: #57b2f3;
+        justify-content: center;
+        font-size: 14px;
+        margin-top: 1vw;
+        span {
+          border-bottom: 1px solid;
+          font-size: 17px;
+        }
+        .long {
+          margin-bottom: 10vw;
+        }
       }
-      .long{
-        margin-bottom: 10vw;
-      }
     }
-  }
   }
 }
 </style>
