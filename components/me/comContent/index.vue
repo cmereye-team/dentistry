c74cb887c3de3695f0a96e2515bb68b37c4834e7
9777777 <zbb1064212882@126.com>
2023-07-27 16:32:28 +0800

    feat: 关于我们新增证书

--- components/me/comContent/index.vue
+++ components/me/comContent/index.vue
@@ -1,40 +1,43 @@
 <template>
-   <div class="bg-white comContent main-container pb-0 py-xs-2">
+  <div class="bg-white comContent main-container pb-0 py-xs-2">
     <div class="meun">
       <p><nuxt-link style="color: #000; text-decoration: none;" to="/">首頁</nuxt-link></p>
       <p style="margin:0 0.5vw">></p>
       <p style="color:#57B2F3">關於我們</p>
     </div>
     <div class="flex justify-center texts">
-        <h2>公司簡介</h2>
-        <img src="https://static.cmereye.com/static/lkximg/image/about-me/serve/Vector.png" alt=""  >
-        <h3>希瑪牙科</h3>
-        <p class="serve_cons" >
-          為香港希瑪眼科醫療控股有限公司<br>
-          （股份代號：3309）旗下的牙科中心，<br>
-          分佈港九新界，位於中環、尖沙咀、銅鑼灣、灣仔、沙田、鑽石山及觀塘。
-        </p>
+      <h2>公司簡介</h2>
+      <img src="https://static.cmereye.com/static/lkximg/image/about-me/serve/Vector.png" alt="">
+      <h3>希瑪牙科</h3>
+      <p class="serve_cons">
+        為香港希瑪眼科醫療控股有限公司<br>
+        （股份代號：3309）旗下的牙科中心，<br>
+        分佈港九新界，位於中環、尖沙咀、銅鑼灣、灣仔、沙田、鑽石山及觀塘。
+      </p>
     </div>
-     <div class="city">
-        <img src="https://static.cmereye.com/static/lkximg/image/me/CitySkyline.png" alt=""  >
-      </div>
-      <div class="textsd">
-        <p class="ya" >
-          希瑪牙科提供<br></p>
-        <h3>全方位牙科及美容牙科服務</h3>
-        <p class="ya_con" >
-          由洗牙、補牙，牙齒美白等治療，<br>
-          幫助客人回復自信笑容。
-        </p>
-      </div>
-       <div class="textsd" style="padding-top:1vw">
-        <p class="ya yas" >
-          中心秉持<br></p>
-        <h3>「整體分析」的原則 </h3>
-        <p class="ya_con" >
-          根據客人的實際口腔健康狀況，制定嚴格且針對性的治療。同時，我們亦注重<br>口腔衛生教育，希望透過推廣牙齒健康的重要性，提高大眾對口腔護理的關注。
-        </p>
-      </div>
+    <div class="awards">
+      <img src="https://static.cmereye.com/imgs/2023/07/084b7e4b0c0fad67.png" alt="奖项">
+    </div>
+    <div class="city">
+      <img src="https://static.cmereye.com/static/lkximg/image/me/CitySkyline.png" alt="">
+    </div>
+    <div class="textsd">
+      <p class="ya">
+        希瑪牙科提供<br></p>
+      <h3>全方位牙科及美容牙科服務</h3>
+      <p class="ya_con">
+        由洗牙、補牙，牙齒美白等治療，<br>
+        幫助客人回復自信笑容。
+      </p>
+    </div>
+    <div class="textsd" style="padding-top:1vw">
+      <p class="ya yas">
+        中心秉持<br></p>
+      <h3>「整體分析」的原則 </h3>
+      <p class="ya_con">
+        根據客人的實際口腔健康狀況，制定嚴格且針對性的治療。同時，我們亦注重<br>口腔衛生教育，希望透過推廣牙齒健康的重要性，提高大眾對口腔護理的關注。
+      </p>
+    </div>
   </div>
 </template>
 <script>
@@ -48,107 +51,147 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+.texts {
+  text-align: center;
 
-.texts{
-    text-align: center;
-    h2{
+  h2 {
     font-weight: 300;
     color: #79828D;
     margin-bottom: 0vw;
   }
-  image{
+
+  image {
     text-align: center;
   }
 }
+
 //mb
 @media only screen and (max-width: 760px) {
-  .textsd{
-   margin: 8vw 0;
-   text-align: center;
-   h3{
-    padding: 0;
+  .textsd {
+    margin: 8vw 0;
+    text-align: center;
 
-   }
-   .ya{
-    color: #57B2F3;
-    margin-bottom: 0.5vw;
-   }
+    h3 {
+      padding: 0;
+
+    }
+
+    .ya {
+      color: #57B2F3;
+      margin-bottom: 0.5vw;
+    }
   }
-  .meun{
-    padding-left:6vw;
+
+  .meun {
+    padding-left: 6vw;
     display: flex;
-    
+
   }
-  .ya_con{
+
+  .ya_con {
     text-align: center;
     color: #9BA5B1;
     font-size: 13px;
     padding: 0 20vw;
   }
-  .city{
+
+  .awards {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+
+    &>img {
+      width: 80%;
+    }
+  }
+
+  .city {
     display: flex;
     text-align: center;
     justify-content: center;
-    img{
-      margin:5vw;
+
+    img {
+      margin: 5vw;
       width: 92%;
       height: 92%;
     }
   }
-    h3{
+
+  h3 {
     color: #57B2F3;
-    padding-top:5vw;
+    padding-top: 5vw;
     font-weight: 600;
 
   }
- .serve_cons{
-        text-align: center;
-        padding: 2vw;
-        color: #57B2F3;
+
+  .serve_cons {
+    text-align: center;
+    padding: 2vw;
+    color: #57B2F3;
+  }
 }
-}
+
 //pc
 @media only screen and (min-width: 760px) {
-  .textsd{
-   text-align: center;
-  //  margin: 8vw 0;
-    .ya{
-         color:#57B2F3;
-         margin-bottom:0.5vw;
-         font-size: 1.3rem;
+  .awards {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+
+    &>img {
+      width: 80%;
     }
-  h3{
-    padding:0;
-    font-size: 1.5rem;
   }
-  image{
+
+  .textsd {
     text-align: center;
+
+    //  margin: 8vw 0;
+    .ya {
+      color: #57B2F3;
+      margin-bottom: 0.5vw;
+      font-size: 1.3rem;
+    }
+
+    h3 {
+      padding: 0;
+      font-size: 1.5rem;
+    }
+
+    image {
+      text-align: center;
+    }
   }
-}
-  .texts{
-    h2{
-    font-size: 2.3rem;
-    font-weight: 300;
-    color: #79828D;
-    margin-bottom: 0vw;
-  }
-    h3{
+
+  .texts {
+    h2 {
+      font-size: 2.3rem;
+      font-weight: 300;
+      color: #79828D;
+      margin-bottom: 0vw;
+    }
+
+    h3 {
       text-align: center;
       color: #57B2F3;
       font-size: 1.5rem;
       padding-top: 80px;
+    }
   }
-  }
-  .comContent{
+
+  .comContent {
     width: calc(100% - 120px);
   }
-  .yas{
+
+  .yas {
     padding-top: 90px
   }
-  .meun{
-    padding-left:100px;
+
+  .meun {
+    padding-left: 100px;
     display: flex;
-    p{
+
+    p {
       font-size: 1rem;
       font-family: 'Noto Sans CJK TC';
       font-style: normal;
@@ -159,36 +202,40 @@ export default {
 
     }
   }
-  .ya_con{
+
+  .ya_con {
     text-align: center;
     color: #9BA5B1;
     font-size: 1.3rem;
   }
-  .city{
+
+  .city {
     display: flex;
     text-align: center;
     justify-content: center;
-    img{
+
+    img {
       padding: 150px;
     }
   }
-    h3{
-      color: #57B2F3;
-       padding-top:5vw;
-      }
-    .serve_cons{
+
+  h3 {
+    color: #57B2F3;
+    padding-top: 5vw;
+  }
+
+  .serve_cons {
     text-align: center;
     color: #57B2F3;
     font-size: 1.3rem;
-    }
+  }
 }
 </style>
 <style lang="scss" >
 //pc
 @media only screen and (min-width: 760px) {
-.comContent{
-        margin-top: 56px !important;
+  .comContent {
+    margin-top: 56px !important;
   }
 }
-
 </style>
