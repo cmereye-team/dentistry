65eb35c5ebc18f98998b0297d6a2576ef8089679
9777717 <zbb1064212882@126.com>
2023-12-08 19:37:04 +0800

    fix: 更新轮播

--- components/slider/InnerSlider.vue
+++ components/slider/InnerSlider.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <div class="inner_slider">
-      <div v-swiper:mySwiper="swiperOption" class="swiperWrap">
+    <!-- <div class="inner_slider">
+      <div v-swiper:mySwiper="swiperOption">
         <div class="swiper-wrapper">
           <div class="swiper-slide w-full" v-for="(imgItem, index) in SliderImgList" :key="index">
             <div v-if="imgItem.type === 2">
@@ -12,7 +12,15 @@
         </div>
       </div>
       <div class="swiper-pagination"></div>
-    </div>
+    </div> -->
+    <el-carousel indicator-position="outside">
+      <el-carousel-item v-for="item in SliderImgList" :key="item">
+        <div v-if="item.type === 2">
+          <a :href="item.link">
+            <img class="w-full img_item" :src="item.url" :srcset="item.srcset" /></a>
+        </div>
+      </el-carousel-item>
+    </el-carousel>
   </div>
 </template>
 <script>
@@ -27,20 +35,20 @@ export default {
   },
   data() {
     return {
-      swiperOption: {
-        loop: true,
-        autoplay: {
-          delay: 2500,
-          disableOnInteraction: false,
-        },
-        centeredSlides: true,
-        spaceBetween: 50,
-        slidesPerView: "1",
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-      },
+      // swiperOption: {
+      //   loop: true,
+      //   autoplay: {
+      //     delay: 2500,
+      //     disableOnInteraction: false,
+      //   },
+      //   centeredSlides: true,
+      //   spaceBetween: 50,
+      //   slidesPerView: "1",
+      //   pagination: {
+      //     el: ".swiper-pagination",
+      //     clickable: true,
+      //   },
+      // },
     };
   },
 };
@@ -48,34 +56,47 @@ export default {
 <style scoped lang="scss">
 @media only screen and (min-width: 768px) {
   .img_item {
-    width: 100%;
+    width: 503px;
     height: 281px;
+    border-radius: 25px;
   }
-
-  .swiper-pagination {
-    position: absolute;
-    width: 100%;
-    left: 0;
-    transform: translate(15%, 0);
-    bottom: -12%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  ::v-deep .swiper-pagination-bullet {
-    width: 12px;
+  ::v-deep .el-carousel__button {
     height: 12px;
-    background-color: #57B2F3;
-    margin: 0 5px
+    width: 12px;
+    border-radius: 100%;
+    background: #57B2F3;
   }
 
-  ::v-deep .swiper-pagination-bullet-active {
-    width: 15px;
-    height: 15px;
-  }
+//   .swiper-pagination {
+//     position: absolute;
+//     width: 100% !important;
+//     left: 0;
+//     transform: translate(15%, 0);
+//     bottom: -12%;
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//   }
+
+//   ::v-deep .swiper-pagination-bullet {
+//     width: 12px !important;
+//     height: 12px !important;
+//     background-color: #57B2F3 !important;
+//     margin: 0 5px !important;
+//     transform: scale(1) !important;
+//   }
+
+//   ::v-deep .swiper-pagination-bullet-active {
+//     width: 15px !important;
+//     height: 15px !important;
+//   }
+
+//   ::v-deep .swiper-pagination-bullets-dynamic .swiper-pagination-bullet {
+//     transform: scale(1) !important;
+//     position: unset !important;
+//     left: auto;
+//   }
 }
 
-@media only screen and (max-width: 768px) {
-}
+@media only screen and (max-width: 768px) {}
 </style>
