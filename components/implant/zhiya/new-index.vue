5e73f40f386681b83518819f2ce32e05595affda
9777717 <zbb1064212882@126.com>
2023-12-20 18:24:15 +0800

    fix: 间距权重1

--- components/implant/zhiya/new-index.vue
+++ components/implant/zhiya/new-index.vue
@@ -154,7 +154,7 @@ export default {
   }
   ::v-deep .interval  {
     &>div:nth-child(1) {
-      bottom: 305px !important;
+      bottom: 270px !important;
     }
     .content {
       &>div:nth-child(1){
